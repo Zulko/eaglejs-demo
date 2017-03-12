8006c17c34fdbf91a0cadb7e00d511e9437d6364
Valentin Zulkower <valentin.zulkower@gmail.com>
2017-03-12 16:37:56 +0000

    First commit

---
+++ src/slideshows/slideshows.js
@@ -0,0 +1,11 @@
+// This is the list of all slideshows that will be registered on the
+// website, don't forget to register your new slideshows in the list:
+export default {
+  list: [
+    require('./introducing-eagle/IntroducingEagle'),
+    require('./eagle-recipes/EagleRecipes'),
+    require('./eagle-recipes/DemoSlideshow'),
+    require('./themes-slideshow/ThemesSlideshow'),
+    require('./first-slideshow/FirstSlideshow')
+  ]
+}
